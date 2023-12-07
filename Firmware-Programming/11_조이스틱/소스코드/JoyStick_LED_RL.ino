6836512366af8e7911f18a1125e9ab01d5e2fd01
YouHyunji <54940615+YouHyunji@users.noreply.github.com>
2023-12-07 16:21:19 +0900

    조이스틱 실습 소스코드 수정
    조이스틱 좌우로 움직였을 때, LED 출력

--- Firmware-Programming/11_조이스틱/소스코드/JoyStick_LED_RL.ino
+++ Firmware-Programming/11_조이스틱/소스코드/JoyStick_LED_RL.ino
@@ -6,30 +6,34 @@ int Analog_Y = A1;  // 조이스틱 Y축
 int Digital_Z = 3;  // 스위치
 int LED_Red = 13;
 int LED_Green = 12;
+
+int pinLED_Switch = 11; // 투명 LED (스위치)
   
 void setup() {
  Serial.begin(9600);
  pinMode(Analog_X, INPUT);
  pinMode(Analog_Y, INPUT);
  pinMode(Digital_Z, INPUT_PULLUP);
+ 
  pinMode(LED_Red, OUTPUT);
  pinMode(LED_Green, OUTPUT);
+ pinMode(pinLED_Switch, OUTPUT);
 }
 void loop()
 {
    int val_X_A0;
    int val_Y_A1;
-   int val_Z_13;
+   int val_Z_3;
    val_X_A0 = analogRead(Analog_X);   // 가변저항 : 아날로그
    val_Y_A1 = analogRead(Analog_Y);   // 가변저항 : 아날로그
-   val_Z_13 = digitalRead(Digital_Z);   // 스위치 : 디지털
+   val_Z_3 = digitalRead(Digital_Z);   // 스위치 : 디지털
    
    Serial.print("X : ");
    Serial.print(val_X_A0);
    Serial.print(", Y : ");
    Serial.print(val_Y_A1); 
    Serial.print(", Z : ");
-   Serial.println(val_Z_13); 
+   Serial.println(val_Z_3); 
    delay(200);
 
    if(val_X_A0 < 1024 && val_X_A0 >= 0) {
@@ -50,5 +54,14 @@ void loop()
       digitalWrite(LED_Red, LOW);
       digitalWrite(LED_Green, LOW);
     }
+
+  // 스위치 눌렀을 때 LED
+    if(val_Z_3 == LOW) {   // 0일때 ( 눌렀을 때 :PULLUP )
+    Serial.println("Switch = HIGH");
+    digitalWrite(pinLED_Switch, HIGH);    // 스위치 클릭 LED 켜짐 (흰색)
+  }
+  else {
+    digitalWrite(pinLED_Switch, LOW);
+  }
    
 }
